950107e53d4f202bb4e45af5e492b6259bf15c62
John Soto <soto.ja415@gmail.com>
2024-02-03 16:52:40 -0500

    Refactor: include isLive to user

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -3,5 +3,10 @@ import { db } from "@/lib/db";
 export const getUserByUsername = async (username: string) => {
   return await db.user.findUnique({
     where: { username },
+    include: {
+      stream: {
+        select: { isLive: true },
+      },
+    },
   });
 };
