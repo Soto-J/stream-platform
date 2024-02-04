4a9779f5e8454397ba41a133a9b0874ee1e9741a
John Soto <soto.ja415@gmail.com>
2024-02-04 18:28:38 -0500

    Style: Removed empty space in class

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex  h-full flex-col items-center justify-center space-y-6">
+    <div className="flex h-full flex-col items-center justify-center space-y-6">
       <Logo />
       {children}
     </div>
