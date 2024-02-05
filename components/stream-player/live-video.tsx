b4c0fe11d245d8dd699126d2b9fe0e708fc46bd6
John Soto <soto.ja415@gmail.com>
2024-02-04 19:18:00 -0500

    Feat: Created fullscreen control component

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+import { useEventListener } from "usehooks-ts";
 
 import { Participant, Track } from "livekit-client";
 import { useTracks } from "@livekit/components-react";
+
+import { FullscreenControl } from "./fullscreen-control";
 
 type LiveVideoProps = {
   participant: Participant;
@@ -12,6 +16,25 @@ type LiveVideoProps = {
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const onToggleFullscreen = () => {
+    if (isFullscreen) {
+      document.exitFullscreen();
+    } else if (wrapperRef.current) {
+      wrapperRef.current.requestFullscreen();
+    }
+  };
+
+  const handleFullscreenChange = () => {
+    const isCurrentlyFullscreen = document.fullscreenElement !== null;
+    setIsFullscreen(isCurrentlyFullscreen);
+  };
+
+  // Handles when the user exits fullscreen then re-enters fullscreen
+  // TypeError: Failed to execute 'exitFullscreen' on 'Document': Document not active.
+  useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
 
   useTracks([Track.Source.Camera, Track.Source.Microphone])
     .filter((track) => track.participant.identity === participant.identity)
@@ -24,6 +47,14 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   return (
     <div ref={wrapperRef} className="relative flex h-full">
       <video ref={videoRef} width="100%" />
+      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+        <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <FullscreenControl
+            isFullscreen={isFullscreen}
+            onToggle={onToggleFullscreen}
+          />
+        </div>
+      </div>
     </div>
   );
 };
