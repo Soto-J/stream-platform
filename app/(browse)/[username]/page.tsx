f717d032a2e94903a7ee3ff62fe9da2be6eb4c1e
John Soto <soto.ja415@gmail.com>
2024-02-10 17:55:35 -0500

    Update

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -5,32 +5,36 @@ import { getUserByUsername } from "@/lib/user-service";
 import { isBlockedByUser } from "@/lib/block-service";
 
 import { Actions } from "./_components/actions";
+import { StreamPlayer } from "@/components/stream-player";
 
 type UserPageProps = {
-  params: {
-    username: string;
-  };
+  params: { username: string };
 };
 
 const UserPage = async ({ params }: UserPageProps) => {
   const user = await getUserByUsername(params.username);
 
-  if (!user) {
+  if (!user || !user.stream) {
     notFound();
   }
 
-  const isFollowing = await isFollowingUser(user.id);
-  const isBlockedByThisUser = await isBlockedByUser(user.id);
+  const isBlocked = await isBlockedByUser(user.id);
 
-  if (isBlockedByThisUser) {
+  if (isBlocked) {
     return notFound();
   }
 
+  const isFollowing = await isFollowingUser(user.id);
+
   return (
     <div className="flex flex-col gap-y-4">
-      <p>User: {user.username}</p>
-      <p>Is Following: {`${isFollowing}`}</p>
-      <p>User: {user.username}</p>
+      <div>
+        <StreamPlayer
+          user={user}
+          stream={user.stream}
+          isFollowing={isFollowing}
+        />
+      </div>
 
       <Actions userId={user.id} isFollowing={isFollowing} />
     </div>
