3b47822ac931a1c48fa3753fe10cbdc56afa28a3
John Soto <soto.ja415@gmail.com>
2024-02-12 17:52:46 -0500

    Update

--- app/(dashboard)/u/[username]/community/page.tsx
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -1,5 +1,25 @@
-function CommunityPage() {
-  return <div>CommunityPage</div>;
+import { DataTable } from "./_components/data-table";
+import { type Payment, columns } from "./_components/columns";
+
+async function getData(): Promise<Payment[]> {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ];
 }
 
-export default CommunityPage;
+export default async function CommunityPage() {
+  const data = await getData();
+
+  return (
+    <div className="p-6">
+      <h1 className="mb-4 text-2xl font-bold">Community settings</h1>
+
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
