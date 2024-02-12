9b7680fdf91a2c463a7c38ca6e891acd77526ee5
John Soto <soto.ja415@gmail.com>
2024-02-11 20:28:36 -0500

    Update

--- app/error.tsx
+++ app/error.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
-export default function NotFoundPage() {
+export default function ErrorPage() {
   return (
     <div className="flex h-full flex-col items-center justify-center space-y-4 text-muted-foreground">
       <h1 className="text-4xl">404</h1>
